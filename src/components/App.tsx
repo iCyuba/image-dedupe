3754b59ddc2960726631e02fc1f9cfdcb9582231
iCyuba <48867623+iCyuba@users.noreply.github.com>
2023-09-16 21:25:41 +0200

    Added buttons to switch between the 2 views

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,28 @@
-// import Duplicates from "./Duplicates";
+import { useState } from "react";
+import Duplicates from "./Duplicates";
 import Editor from "./Editor";
 
+enum View {
+  Duplicates,
+  Editor,
+}
+
 function App() {
+  const [view, setView] = useState(View.Duplicates);
+
   return (
     <>
       <h1>Image Utils</h1>
 
-      {/* <Duplicates /> */}
-      <Editor />
+      {view === View.Duplicates ? (
+        <button onClick={() => setView(View.Editor)}>Go to editor</button>
+      ) : (
+        <button onClick={() => setView(View.Duplicates)}>
+          Go to duplicates
+        </button>
+      )}
+
+      {view === View.Duplicates ? <Duplicates /> : <Editor />}
     </>
   );
 }
